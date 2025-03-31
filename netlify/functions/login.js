7a7f2250576c1ae02e0f63a4244a4ffd7a0ba4b6
portgasdylan <dylanmaxwell579@gmail.com>
2025-03-31 16:51:37 -0700

    Added Cookie to track Sessions

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,5 +1,6 @@
 const connectDB = require("./db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 exports.handler = async (event, context) => {
@@ -24,8 +25,20 @@ exports.handler = async (event, context) => {
       return { statusCode: 401, body: "Invalid username or password." };
     }
 
+    //Sign a JWT
+    const token = jwt.sign(
+      { id: used._id, username: user.username },
+      process.env.JWT_SECRET || 'default_secret_key',
+      { expiresIn: "2h" }
+    );
+
+    // Set the token as an HTTP-only cookie
     return {
       statusCode: 200,
+      headers: {
+        "Set-Cookie": `token=${token}; HttpOnly; Path=/; Max-Age=${2 * 3600}`,
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ message: "Login successful!" }),
     };
   } catch (error) {
